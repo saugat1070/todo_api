5f9085cc611d133bbd2c59523f209c1be3d18422
saugat1070 <saugatgiri1070@gmail.com>
2025-07-07 16:10:28 +0545

    database connection completed

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require("express")
-
+// const express = require("express")
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.listen(3000,()=>{
-    console.log(`server is run at 3000`);
+mongoose.connect(process.env.DB_URL
+).then(()=>{
+    console.log("DataBase is connected!!");
+}).catch(()=>{
+    console.log("Something error on database connectioni");
 })
+
+app.listen(3000, () => {
+  console.log(`server is run at 3000`);
+});
+
+// https://github.com/saugat1070
