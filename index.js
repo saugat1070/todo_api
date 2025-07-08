8ea6871b139f7d36d29c61e5b3ec1b7b066ad820
saugat1070 <saugatgiri1070@gmail.com>
2025-07-08 14:14:16 +0545

    day2 backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import User from "./userModel.js";
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -17,6 +18,23 @@ mongoose.connect(process.env.DB_URL
     console.log("DataBase is connected!!");
 }).catch(()=>{
     console.log("Something error on database connectioni");
+})
+
+app.post("/",async(req,res)=>{
+    const {email,password,fullName} = req.body;
+    const userCreate = await User.create({
+        ...req.body
+    })
+    if(!userCreate){
+        res.json({
+            message : "failed in creation during database"
+        });
+        return;
+    }
+    res.json({
+        message : "user register successfull"
+    })
+
 })
 
 app.listen(3000, () => {
--- userModel.js
+++ userModel.js
@@ -1,15 +1,23 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    email : {
-        type : String,
-        required: true,
-        null : false,
-        unique : true
-    },
-    fullName :{
-        type : String,
-        required : true,
-        null : false
-    }
-})
+  email: {
+    type: String,
+    required: true,
+    null: false,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    null: false,
+  },
+  password: {
+    type: String,
+    required: true,
+    null: false,
+  },
+});
+
+const User = mongoose.model("User",userSchema);
+export default User;
