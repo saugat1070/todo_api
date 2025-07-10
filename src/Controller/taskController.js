ed59d9548eefa19488f5e747d9fd479c61ddd9a3
saugat1070 <saugatgiri1070@gmail.com>
2025-07-10 14:57:45 +0545

    taskcreate controller

---
+++ src/Controller/taskController.js
@@ -0,0 +1,69 @@
+import User from "../Model/userModel";
+import SubTask from "../Model/subtaskModel";
+import Task from "../Model/taskModel";
+
+
+export const TaskCreate = async (req,res)=>{
+    const {title} = req.body;
+    const userId = req.user.id;
+    if(!title || !userId){
+        res.status(400).json({
+            message : "title or userId is missing"
+        });
+        return;
+    }
+
+    const taskCreate = await Task.create({
+        title:title,
+        createdBy : userId
+    });
+    if(!taskCreate){
+        res.status(400).json({
+            message : "task creation failed in database"
+        });
+        return;
+    }
+    res.status(200).json({
+        message : "task complete successfull"
+    })
+}
+
+export const subTaskCreate = (req,res)=>{
+    const {subtitle,description} = req.body;
+    if(!subtitle || !description){
+        res.status(400).json({
+            message : "subtitle,description must be provided"
+        });
+        return;
+    }
+    let imageName = ""
+    if(req.file){
+        imageName = req.file.filename;
+    }
+
+    const userId = req.user.id;
+    const {titleId} = req.params;
+    if(!userId || !titleId){
+        res.status(400).json({
+            message : "userId and titleId must be provided"
+        });
+        return;
+    }
+
+    const subTaskCreate = SubTask.create({
+        title : titleId,
+        subtitle : subtitle,
+        description : description,
+        image : imageName
+    });
+    if(!subTaskCreate){
+        res.json({
+            message : "Something fail during subTaskCreation"
+        });
+    return;
+    }
+
+    res.status(200).json({
+        message : "subTask created"
+    })
+}
